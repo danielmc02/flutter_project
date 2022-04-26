20b6f33cad6e22dfbffe1cdb12d0c6c61c4b88cf
Snippy <digity63@gmail.com>
2022-04-26 13:15:01 -0700

    updated

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -60,8 +60,17 @@ class _checkBoxState extends State<checkBox> {
 bool? isChecked = false;
   @override
   Widget build(BuildContext context) {
-    return Checkbox(value: isChecked, onChanged: (bool? value) {setState(() {
+    return Checkbox
+    (value: isChecked, onChanged: (bool? value) 
+    {
+      Listener(onPointerDown: (PointerDownEvent)
+      {print("dsf")}
+    setState(
+      () 
+    {
       isChecked = value!;
-    });} );
+    }
+    )
+    ;} );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.purple,
         primarySwatch: Colors.blue,
+        
       ),
       home: const LoginScreen(),
     );
